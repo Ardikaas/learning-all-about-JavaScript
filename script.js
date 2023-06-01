78a430ea793b821dac43c38c69252047e2a9e3d9
Ardikaas <ardikaajisetiawan@gmail.com>
2023-06-01 21:54:45 +0700

    make simple onclick function

---
+++ script.js
@@ -0,0 +1,30 @@
+function one(){
+  document.getElementById("ansbar").innerHTML += "1";
+}
+function two(){
+  document.getElementById("ansbar").innerHTML += "2";
+}
+function three(){
+  document.getElementById("ansbar").innerHTML += "3";
+}
+function four(){
+  document.getElementById("ansbar").innerHTML += "4";
+}
+function five(){
+  document.getElementById("ansbar").innerHTML += "5";
+}
+function six(){
+  document.getElementById("ansbar").innerHTML += "6";
+}
+function seven(){
+  document.getElementById("ansbar").innerHTML += "7";
+}
+function eight(){
+  document.getElementById("ansbar").innerHTML += "8";
+}
+function nine(){
+  document.getElementById("ansbar").innerHTML += "9";
+}
+function zero(){
+  document.getElementById("ansbar").innerHTML += "0";
+}
